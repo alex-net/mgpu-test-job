3a353395f43912348d2ad4a3e8b6d695edc03201
Alex Net <alexnet2004@gmail.com>
2023-02-16 01:39:21 +0300

    Скрыли недоступный для анонима пункт меню

--- site/views/layouts/main.php
+++ site/views/layouts/main.php
@@ -41,7 +41,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Доки', 'url' => ['/docs/index']],
+            ['label' => 'Доки', 'url' => ['/docs/index'], 'visible' => !Yii::$app->user->isGuest],
             Yii::$app->user->isGuest
                 ? ['label' => 'Login', 'url' => ['/site/login']]
                 : '<li class="nav-item">'
